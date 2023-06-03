796119e3bf9bad45bfc0f1304ea0487078610032
LiranBratt2121 <brattlirannin@gmail.com>
2023-06-04 00:06:39 +0300

    parse_file: Fix code logic

--- parse_file.py
+++ parse_file.py
@@ -4,15 +4,15 @@ import csv
 with open('webpage.html', 'r', encoding='utf-8') as file:
     html_code = file.read()
 
-    name_matches = re.finditer(r'<p\s+class="[^"]*"\s*>(.*?)</p>', html_code, re.DOTALL)
-    message_count_matches = re.finditer(r'<p\s+class="[^"]*text-dark-300[^"]*"\s*>(.*?)</p>', html_code, re.DOTALL)
-    experience_count_matches = re.finditer(r'<p\s+class="[^"]*hidden[^"]*text-dark-300[^"]*"\s*>(.*?)</p>', html_code, re.DOTALL)
-    level_matches = re.finditer(r'<div\s+class="[^"]*text-white[^"]*"\s*>(.*?)</div>', html_code, re.DOTALL)
+    name_matches = re.findall(r'<p\s+class="[^"]*"\s*>(.*?)</p>', html_code, re.DOTALL)
+    message_count_matches = re.findall(r'<p\s+class="[^"]*text-dark-300[^"]*"\s*>(.*?)</p>', html_code, re.DOTALL)
+    experience_count_matches = re.findall(r'<p\s+class="[^"]*text-dark-300[^"]*"\s*>(.*?)</p>', html_code, re.DOTALL)
+    level_matches = re.findall(r'<div\s+class="[^"]*text-white[^"]*"\s*>(.*?)</div>', html_code, re.DOTALL)
 
-    names = [match.group(1).strip() for match in name_matches]
-    message_counts = [match.group(1).strip() for match in message_count_matches]
-    experience_counts = [match.group(1).strip() for match in experience_count_matches]
-    levels = [match.group(1).strip() for match in level_matches]
+    names = [match.strip() for match in name_matches]
+    message_counts = [match.strip() for match in message_count_matches]
+    experience_counts = [match.strip() for match in experience_count_matches]
+    levels = [match.strip() for match in level_matches]
 
     accounts = zip(names, message_counts, experience_counts, levels)
 
